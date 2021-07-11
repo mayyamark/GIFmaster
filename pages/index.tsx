7b7d15ed3dcfeb1a555b36789ee3156a6c93bcb8
mayya.markova <mayya.markova@gmail.com>
2021-07-11 21:08:32 +0300

    Add env variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { GetStaticPropsResult } from 'next';
+import { AppProps } from 'next/app';
 import HomePage from '../src/components/pages/HomePage/HomePage';
 
-export default function Home(): JSX.Element {
-  return <HomePage />;
+interface Props {
+  url: string;
+}
+
+interface HomeAppProps extends AppProps {
+  url: string;
+}
+
+export default function Home(props: HomeAppProps): JSX.Element {
+  return <HomePage {...props} />;
+}
+export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
+  return {
+    props: {
+      url: `${process.env.GIPHY_QUERY_BASE_URL}/trending?api_key=${process.env.GIPHY_API_KEY}`,
+    },
+  };
 }
