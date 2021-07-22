5228ee419e16db83aa7411bcac64bee25a3daf5b
mayya.markova <mayya.markova@gmail.com>
2021-07-22 16:05:27 +0300

    Remove actions.ts and storage.ts

--- src/utils/storage.ts
+++
@@ -1,5 +0,0 @@
-export const getItem = (itemKey: string): string | null =>
-  localStorage.getItem(itemKey);
-
-export const setItem = (itemKey: string, itemValue: string): void =>
-  localStorage.setItem(itemKey, itemValue);
