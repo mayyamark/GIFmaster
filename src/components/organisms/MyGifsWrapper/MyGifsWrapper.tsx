3a0ed9be04caf96519e2a6db316b8cd27f9e5f1d
mayya.markova <mayya.markova@gmail.com>
2021-08-01 08:06:10 +0300

    Fix Loader layout in MyGifsWrapper

--- src/components/organisms/MyGifsWrapper/MyGifsWrapper.tsx
+++ src/components/organisms/MyGifsWrapper/MyGifsWrapper.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import NoGifsToShow from '@app/components/molecules/NoGifsToShow/NoGifsToShow';
 import GifsLayout from '@app/components/organisms/GifsLayout/GifsLayout';
+import Layout from '@app/components/molecules/Layout/Layout';
 import Loader from '@app/components/atoms/Loader/Loader';
 
 interface Props {
@@ -24,7 +25,11 @@ const MyGifsWrapper: React.FC<Props> = ({
   }, [gifIds, setEndpoint, getEndpointUrl]);
 
   if (endpoint === null) {
-    return <Loader showLoader={true} />;
+    return (
+      <Layout>
+        <Loader showLoader={true} />;
+      </Layout>
+    );
   }
 
   return endpoint === '' ? (
